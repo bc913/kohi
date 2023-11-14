3f999ab250f2a54116fa9d35bbe8a28cfe6f1e64
Burak Can <bc913dev@outlook.com>
2023-11-13 21:20:22 -0500

    Test win platform impl

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,5 +1,7 @@
 #include <core/logger.h>
 #include <core/asserts.h>
+// TODO: Test
+#include <platform/platform.h>
 
 int main(void) {
     KFATAL("A test message: %f", 3.14f);
@@ -9,6 +11,13 @@ int main(void) {
     KDEBUG("A test message: %f", 3.14f);
     KTRACE("A test message: %f", 3.14f);
 
-    KASSERT(1 == 0);
+    platform_state state;
+    if (platform_startup(&state, "Kohi engine Testbed", 100, 100, 1280, 720)) {
+        while (TRUE) {  // mimics app main loop
+            platform_pump_messages(&state);
+        }
+    }
+    platform_shutdown(&state);
+
     return 0;
 }
