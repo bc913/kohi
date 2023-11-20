7537c53f2e8250a796546685d95f1203815be1f9
Burak Can <bc913dev@outlook.com>
2023-11-19 23:22:40 -0500

    Add clock implementation

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -7,6 +7,7 @@
 #include "core/kmemory.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -15,6 +16,7 @@ typedef struct application_state {
     platform_state platform;  // no need to be a pointer
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
@@ -90,7 +92,14 @@ b8 application_create(game* game_inst) {
 }
 
 b8 application_run() {
-    // get_memory_usage_str() is not portable due to strdup function call.
+    // start clock before doing anything else
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
+    f64 running_time = 0;                  // how much time has been accumulated
+    u8 frame_count = 0;                    // how many frames has gone in a second
+    f64 target_frame_seconds = 1.0f / 60;  // target frame rate is 60
+
     KINFO(get_memory_usage_str());
 
     while (app_state.is_running) {
@@ -100,18 +109,44 @@ b8 application_run() {
         }
 
         if (!app_state.is_suspended) {
+            // Update clock and get delta time.
+            clock_update(&app_state.clock);
+            f64 current_time = app_state.clock.elapsed;
+            f64 delta = (current_time - app_state.last_time);
+            f64 frame_start_time = platform_get_absolute_time();
+
             // Update
-            if (!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
+            if (!app_state.game_inst->update(app_state.game_inst, (f32)delta)) {
                 KFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
             }
 
             // Call the game's render routine.
-            if (!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
+            if (!app_state.game_inst->render(app_state.game_inst, (f32)delta)) {
                 KFATAL("Game render failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
+            }
+
+            // Figure out how long the frame took and, if below
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - frame_start_time;
+            running_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+            // If we have remaining time, give it back to the system so we
+            // would not consume system resources unnecessarily
+            if (remaining_seconds > 0) {
+                u64 remaining_ms = (remaining_seconds * 1000);
+
+                // If there is time left, give it back to the OS.
+                b8 limit_frames = FALSE;
+                if (remaining_ms > 0 && limit_frames) {
+                    platform_sleep(remaining_ms - 1);
+                }
+
+                frame_count++;
             }
 
             /*
@@ -124,8 +159,11 @@ b8 application_run() {
             // after any input should be recorded; I.E. before this line.
             // As a safety, input is the last thing to be updated before
             // this frame ends.
-            input_update(0);
+            input_update(delta);
             // this can be done before the update too
+
+            // Update last time
+            app_state.last_time = current_time;
         }
     }
 
---
+++ engine/src/core/clock.h
@@ -0,0 +1,21 @@
+#ifndef KOHI_1700440543_CLOCK_H
+#define KOHI_1700440543_CLOCK_H
+
+#include "defines.h"
+
+typedef struct clock {
+    f64 start_time;  // when the clock is started
+    f64 elapsed;     // secs
+} clock;
+
+// Updates the provided clock. Should be called just before checking elapsed time.
+// Has no effect on non-started clocks.
+void clock_update(clock* clock);
+
+// Starts the provided clock. Resets elapsed time.
+void clock_start(clock* clock);
+
+// Stops the provided clock. Does not reset elapsed time.
+void clock_stop(clock* clock);
+
+#endif
