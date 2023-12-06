fd79881200c9059fd940709fdc9c58b32cef697b
Burak Can <bc913dev@outlook.com>
2023-12-06 17:03:57 -0500

    Add linear allocator

---
+++ engine/src/memory/linear_allocator.h
@@ -0,0 +1,19 @@
+#ifndef KOHI_1701897303_LINEAR_ALLOCATOR_H
+#define KOHI_1701897303_LINEAR_ALLOCATOR_H
+
+#include "defines.h"
+
+typedef struct linear_allocator {
+    u64 total_size;
+    u64 allocated;
+    void* memory;
+    b8 owns_memory;
+} linear_allocator;
+
+KAPI void linear_allocator_create(u64 total_size, void* memory, linear_allocator* out_allocator);
+KAPI void linear_allocator_destroy(linear_allocator* allocator);
+
+KAPI void* linear_allocator_allocate(linear_allocator* allocator, u64 size);
+KAPI void linear_allocator_free_all(linear_allocator* allocator);
+
+#endif
